269af390c7e11300e19119557d197b6634145e5f
Rituraj Anand <riturajanand91@gmail.com>
2020-09-19 11:53:47 +0530

    Update database.js

--- db/database.js
+++ db/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://riturajanand:riturajanand@cluster0-tzdl4.mongodb.net/login-register?retryWrites=true&w=majority',{
+mongoose.connect('',{
     useNewUrlParser:true,
     useCreateIndex:true,
     useUnifiedTopology: true
